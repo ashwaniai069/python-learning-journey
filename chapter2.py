9b76ab5795961954cd895a48d7ae6877dd06edaa
ASHWANI KUMAR <ashwanimaitry.ml@gmail.com>
2026-06-06 14:43:12 +0530

    Add files via upload

---
+++ chapter2.py
@@ -0,0 +1,48 @@
+#comments
+#this line below is use for printing
+print("hello ash")
+"""hello im ash 
+what are u doing   -> this is multi line comment
+are you ok"""
+print("ashwani jie")
+
+
+
+#variables
+
+lion = 21
+
+print(lion)
+
+banana = 69
+apple = 43
+
+print(banana,apple)
+
+#anything can become variable
+
+#rules for variable
+# 1. variable can't start with number
+# eg.
+
+
+# 2.no space in variables
+
+
+# 3. don't use special charector
+
+#camel case
+ashwaniXage = 12
+print(ashwaniXage)
+
+#pascal case
+AshwaniXage = 12
+
+#snake_case
+ashwani_age = 12
+
+
+a = 2
+b = 4
+c = 3
+print(a,b,c)
